62007554998010bfffd69bd96436afb67aefc172
arimariojesus <arimario.jesus@hotmail.com>
2022-11-28 22:40:04 -0300

    feat: implements socket

--- src/server/index.ts
+++ src/server/index.ts
@@ -3,8 +3,10 @@ import 'module-alias/register';
 import '../config/module-alias';
 
 import path from 'node:path';
+import http from 'node:http';
 import express from 'express';
 import mongoose from 'mongoose';
+import { Server } from 'socket.io';
 import * as dotenv from 'dotenv';
 
 import { router } from './router';
@@ -14,11 +16,13 @@ dotenv.config();
 const PORT = process.env.PORT || 3333;
 const MONGO_URL = process.env.MONGO_URL;
 
+const app = express();
+const server = http.createServer(app);
+export const io = new Server(server);
+
 mongoose
   .connect(MONGO_URL)
   .then(() => {
-    const app = express();
-
     app.use((req, res, next) => {
       res.setHeader('Access-Control-Allow-Origin', '*');
       res.setHeader('Access-Control-Allow-Methods', '*');
@@ -32,7 +36,7 @@ mongoose
     app.use(express.json());
     app.use(router);
 
-    app.listen(PORT, () => {
+    server.listen(PORT, () => {
       console.log(`Server is running on http://localhost:${PORT}`);
     });
   })
